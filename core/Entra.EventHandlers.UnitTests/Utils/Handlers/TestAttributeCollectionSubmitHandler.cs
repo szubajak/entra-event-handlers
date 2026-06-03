0db9d4c3f00418ce0b59e97ac4ff8d043cac63a5
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-06-03 11:41:18 +0200

    test: unit tests for handlers base

---
+++ core/Entra.EventHandlers.UnitTests/Utils/Handlers/TestAttributeCollectionSubmitHandler.cs
@@ -0,0 +1,22 @@
+﻿using Entra.EventHandlers.Abstractions.Events;
+using Entra.EventHandlers.Abstractions.Responses;
+using Entra.EventHandlers.Handlers.Base;
+using Microsoft.Extensions.Logging;
+
+namespace Entra.EventHandlers.UnitTests.Utils.Handlers;
+
+public class TestAttributeCollectionSubmitHandler(ILogger<AttributeCollectionSubmitHandlerBase> logger)
+    : AttributeCollectionSubmitHandlerBase(logger)
+{
+    public HandlerCoreTest CoreTest { get; } = new HandlerCoreTest();
+
+    public AttributeCollectionSubmitResponse ResponseToReturn { get; set; } = new();
+
+    protected override Task<AttributeCollectionSubmitResponse> HandleCore(
+        AttributeCollectionSubmitEvent request,
+        CancellationToken cancellationToken)
+    {
+        CoreTest.Record(cancellationToken);
+        return Task.FromResult(ResponseToReturn);
+    }
+}
---
+++ core/Entra.EventHandlers.UnitTests/Utils/Handlers/TestEmailOtpSendHandler.cs
@@ -0,0 +1,22 @@
+﻿using Entra.EventHandlers.Abstractions.Events;
+using Entra.EventHandlers.Abstractions.Responses;
+using Entra.EventHandlers.Handlers.Base;
+using Microsoft.Extensions.Logging;
+
+namespace Entra.EventHandlers.UnitTests.Utils.Handlers;
+
+public class TestEmailOtpSendHandler(ILogger<EmailOtpSendHandlerBase> logger)
+    : EmailOtpSendHandlerBase(logger)
+{
+    public HandlerCoreTest CoreTest { get; } = new HandlerCoreTest();
+
+    public EmailOtpSendResponse ResponseToReturn { get; set; } = new();
+
+    protected override Task<EmailOtpSendResponse> HandleCore(
+        EmailOtpSendEvent request,
+        CancellationToken cancellationToken)
+    {
+        CoreTest.Record(cancellationToken);
+        return Task.FromResult(ResponseToReturn);
+    }
+}
