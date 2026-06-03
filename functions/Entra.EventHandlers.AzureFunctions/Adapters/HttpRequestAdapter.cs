75e20c7e82d4d635e3a486a09bd6e95111458dba
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-06-03 16:32:12 +0200

    Release: v1.2.0
    * feat: additional nuget for Azure Functions
    
    * chore: update workflows
    
    * chore: fix csproj, add icon

---
+++ functions/Entra.EventHandlers.AzureFunctions/Adapters/HttpRequestAdapter.cs
@@ -0,0 +1,16 @@
+﻿using Entra.EventHandlers.Abstractions.Events;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Text.Json;
+
+namespace Entra.EventHandlers.AzureFunctions.Adapters;
+
+public static class HttpRequestAdapter
+{
+    public static async Task<TEvent> ReadEvent<TEvent>(HttpRequestData req)
+        where TEvent : EntraEvent =>
+        await JsonSerializer.DeserializeAsync<TEvent>(req.Body)
+            ?? throw new InvalidOperationException("Unable to deserialize event.");
+
+    public static Task<EntraEvent> ReadEvent(HttpRequestData req) =>
+        ReadEvent<EntraEvent>(req);
+}
