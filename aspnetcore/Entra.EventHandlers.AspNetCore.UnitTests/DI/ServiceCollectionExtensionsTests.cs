27e99daa2cdd25cd8ec6f53112c78b7ce4780a68
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-06-15 14:23:21 +0200

    test: unit tests coverage, more unit tests

--- aspnetcore/Entra.EventHandlers.AspNetCore.UnitTests/DI/ServiceCollectionExtensionsTests.cs
+++ aspnetcore/Entra.EventHandlers.AspNetCore.UnitTests/DI/ServiceCollectionExtensionsTests.cs
@@ -1,9 +1,12 @@
-﻿using Entra.EventHandlers.AspNetCore.Adapters;
+﻿using Entra.EventHandlers.Abstractions.Interfaces;
+using Entra.EventHandlers.AspNetCore.Abstractions;
+using Entra.EventHandlers.AspNetCore.Adapters;
 using Entra.EventHandlers.AspNetCore.DI;
 using Entra.EventHandlers.Hosting.Resolvers;
 using Entra.EventHandlers.TestHelpers;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 
 namespace Entra.EventHandlers.AspNetCore.UnitTests.DI;
 
@@ -60,4 +63,59 @@ public class ServiceCollectionExtensionsTests
         provider.GetService<IResponseAdapter>()
             .Should().NotBeNull();
     }
+
+    [Theory]
+    [MemberData(nameof(AllEndpointTypes))]
+    public void AddEntraEventHandlers_RegistersEndpoints(Type endpointType)
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        services.AddLogging();
+
+        RegisterAllHandlersAsSubstitutes(services);
+
+        // Act
+        services.AddEntraEventHandlers();
+
+        // Assert
+        var provider = services.BuildServiceProvider();
+
+        provider.GetService(endpointType)
+            .Should().NotBeNull();
+    }
+
+    public static TheoryData<Type> AllEndpointTypes()
+    {
+        var data = new TheoryData<Type>();
+
+        var endpointBase = typeof(EntraEndpointBase);
+
+        var endpoints = endpointBase.Assembly
+            .GetTypes()
+            .Where(t => !t.IsAbstract && endpointBase.IsAssignableFrom(t));
+
+        foreach (var endpoint in endpoints)
+            data.Add(endpoint);
+
+        return data;
+    }
+
+    private static void RegisterAllHandlersAsSubstitutes(IServiceCollection services)
+    {
+        foreach (var handler in GetAllHandlerInterfaces())
+        {
+            services.AddTransient(handler, _ => Substitute.For([handler], []));
+        }
+    }
+
+    public static IEnumerable<Type> GetAllHandlerInterfaces()
+    {
+        var handlerBase = typeof(IEntraEventHandler<,>);
+
+        return handlerBase.Assembly
+            .GetTypes()
+            .Where(t => t.IsInterface && t != handlerBase)
+            .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerBase));
+    }
+
 }
