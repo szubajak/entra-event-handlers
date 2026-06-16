152d45e49e387b64cd48abf608b0020e78237ef0
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-06-16 17:32:31 +0200

    test: unit tests for adapters

---
+++ azurefunctions/Entra.EventHandlers.AzureFunctions.UnitTests/Adapters/RequestAdapterTests.cs
@@ -0,0 +1,84 @@
+﻿using Entra.EventHandlers.Abstractions.Errors;
+using Entra.EventHandlers.AzureFunctions.Adapters;
+using Entra.EventHandlers.AzureFunctions.UnitTests.Utils;
+using Entra.EventHandlers.TestHelpers;
+using FluentAssertions;
+using Microsoft.Azure.Functions.Worker;
+using NSubstitute;
+using System.Text;
+
+namespace Entra.EventHandlers.AzureFunctions.UnitTests.Adapters;
+
+public class RequestAdapterTests
+{
+    private readonly RequestAdapter _sut;
+
+    public RequestAdapterTests()
+    {
+        _sut = new RequestAdapter();
+    }
+
+    [Fact]
+    public async Task ReadEvent_WhenBodyIsEmpty_ThrowsDeserializationException()
+    {
+        // Arrange
+        var ctx = Substitute.For<FunctionContext>();
+        var req = new TestHttpRequestData(ctx, new MemoryStream(Encoding.UTF8.GetBytes("")));
+
+        // Act
+        var act = async () => await _sut.ReadEvent<TestEvent>(req);
+
+        // Assert
+        await act.Should()
+            .ThrowAsync<EntraDeserializationException>()
+            .WithMessage("Request body is empty.");
+    }
+
+    [Fact]
+    public async Task ReadEvent_WhenJsonIsInvalid_ThrowsInvalidJsonException()
+    {
+        // Arrange
+        var ctx = Substitute.For<FunctionContext>();
+        var req = new TestHttpRequestData(ctx, new MemoryStream(Encoding.UTF8.GetBytes("{ invalid json ")));
+
+        // Act
+        var act = async () => await _sut.ReadEvent<TestEvent>(req);
+
+        // Assert
+        await act.Should()
+            .ThrowAsync<EntraDeserializationException>()
+            .WithMessage("Invalid JSON payload.");
+    }
+
+    [Fact]
+    public async Task ReadEvent_WhenJsonDeserializesToNull_ThrowsUnableToDeserialize()
+    {
+        // Arrange
+        var ctx = Substitute.For<FunctionContext>();
+        var req = new TestHttpRequestData(ctx, new MemoryStream(Encoding.UTF8.GetBytes("null")));
+
+        // Act
+        var act = async () => await _sut.ReadEvent<TestEvent>(req);
+
+        // Assert
+        await act.Should()
+            .ThrowAsync<EntraDeserializationException>()
+            .WithMessage("Unable to deserialize event.");
+    }
+
+    [Fact]
+    public async Task ReadEvent_WhenUnexpectedExceptionOccurs_ThrowsFailedToDeserialize()
+    {
+        // Arrange
+        var ctx = Substitute.For<FunctionContext>();
+        var req = new TestHttpRequestData(ctx, new ThrowingStream());
+
+        // Act
+        var act = async () => await _sut.ReadEvent<TestEvent>(req);
+
+        // Assert
+        await act.Should()
+            .ThrowAsync<EntraDeserializationException>()
+            .WithMessage("Failed to deserialize event.");
+    }
+}
---
+++ common/Entra.EventHandlers.TestHelpers/ThrowingStream.cs
@@ -0,0 +1,17 @@
+﻿namespace Entra.EventHandlers.TestHelpers;
+
+public sealed class ThrowingStream : Stream
+{
+    public override int Read(byte[] buffer, int offset, int count) =>
+        throw new IOException("Error!");
+
+    public override bool CanRead => true;
+    public override bool CanSeek => false;
+    public override bool CanWrite => false;
+    public override long Length => throw new NotSupportedException();
+    public override long Position { get => 0; set => throw new NotSupportedException(); }
+    public override void Flush() => throw new NotSupportedException();
+    public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+    public override void SetLength(long value) => throw new NotSupportedException();
+    public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+}
