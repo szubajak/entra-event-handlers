d1cb83cffccb830efa482788d4907dfa322d8629
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-06-25 19:10:12 +0200

    test: more unit tests

--- hosting/Entra.EventHandlers.Hosting.UnitTests/Orchestrators/EntraEventOrchestratorTests.cs
+++ hosting/Entra.EventHandlers.Hosting.UnitTests/Orchestrators/EntraEventOrchestratorTests.cs
@@ -24,21 +24,111 @@ public class EntraEventOrchestratorTests
     }
 
     [Fact]
-    public async Task DispatchAsync_Success()
+    public async Task DispatchAsync_AttributeCollectionStartEvent_Success()
     {
         // Arrange
         var fixture = new Fixture();
 
         var evt = fixture.Create<AttributeCollectionStartEvent>();
-
         var response = new AttributeCollectionStartResponse();
-
         var cts = new CancellationTokenSource();
 
         var handler = Substitute.For<IEntraEventHandler<AttributeCollectionStartEvent, AttributeCollectionStartResponse>>();
         handler.HandleAsync(evt, cts.Token).Returns(response);
 
         _resolver.Resolve<AttributeCollectionStartEvent, AttributeCollectionStartResponse>()
+            .Returns(handler);
+
+        // Act
+        var result = await _sut.DispatchAsync(evt, cts.Token);
+
+        // Assert
+        result.Should().Be(response);
+    }
+
+    [Fact]
+    public async Task DispatchAsync_AttributeCollectionSubmitEvent_Success()
+    {
+        // Arrange
+        var fixture = new Fixture();
+
+        var evt = fixture.Create<AttributeCollectionSubmitEvent>();
+        var response = new AttributeCollectionSubmitResponse();
+        var cts = new CancellationTokenSource();
+
+        var handler = Substitute.For<IEntraEventHandler<AttributeCollectionSubmitEvent, AttributeCollectionSubmitResponse>>();
+        handler.HandleAsync(evt, cts.Token).Returns(response);
+
+        _resolver.Resolve<AttributeCollectionSubmitEvent, AttributeCollectionSubmitResponse>()
+            .Returns(handler);
+
+        // Act
+        var result = await _sut.DispatchAsync(evt, cts.Token);
+
+        // Assert
+        result.Should().Be(response);
+    }
+
+    [Fact]
+    public async Task DispatchAsync_TokenIssuanceStartEvent_Success()
+    {
+        // Arrange
+        var fixture = new Fixture();
+
+        var evt = fixture.Create<TokenIssuanceStartEvent>();
+        var response = new TokenIssuanceStartResponse();
+        var cts = new CancellationTokenSource();
+
+        var handler = Substitute.For<IEntraEventHandler<TokenIssuanceStartEvent, TokenIssuanceStartResponse>>();
+        handler.HandleAsync(evt, cts.Token).Returns(response);
+
+        _resolver.Resolve<TokenIssuanceStartEvent, TokenIssuanceStartResponse>()
+            .Returns(handler);
+
+        // Act
+        var result = await _sut.DispatchAsync(evt, cts.Token);
+
+        // Assert
+        result.Should().Be(response);
+    }
+
+    [Fact]
+    public async Task DispatchAsync_EmailOtpSendEvent_Success()
+    {
+        // Arrange
+        var fixture = new Fixture();
+
+        var evt = fixture.Create<EmailOtpSendEvent>();
+        var response = new EmailOtpSendResponse();
+        var cts = new CancellationTokenSource();
+
+        var handler = Substitute.For<IEntraEventHandler<EmailOtpSendEvent, EmailOtpSendResponse>>();
+        handler.HandleAsync(evt, cts.Token).Returns(response);
+
+        _resolver.Resolve<EmailOtpSendEvent, EmailOtpSendResponse>()
+            .Returns(handler);
+
+        // Act
+        var result = await _sut.DispatchAsync(evt, cts.Token);
+
+        // Assert
+        result.Should().Be(response);
+    }
+
+    [Fact]
+    public async Task DispatchAsync_PasswordSubmitEvent_Success()
+    {
+        // Arrange
+        var fixture = new Fixture();
+
+        var evt = fixture.Create<PasswordSubmitEvent>();
+        var response = new PasswordSubmitResponse();
+        var cts = new CancellationTokenSource();
+
+        var handler = Substitute.For<IEntraEventHandler<PasswordSubmitEvent, PasswordSubmitResponse>>();
+        handler.HandleAsync(evt, cts.Token).Returns(response);
+
+        _resolver.Resolve<PasswordSubmitEvent, PasswordSubmitResponse>()
             .Returns(handler);
 
         // Act
