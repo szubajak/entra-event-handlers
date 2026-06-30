ac710ac41881119d5b3f1e639541b389eef0f7fa
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-06-30 09:12:23 +0200

    chore: simplify endpoints

--- aspnetcore/Entra.EventHandlers.AspNetCore/Endpoints/AttributeCollectionSubmitEndpoint.cs
+++ aspnetcore/Entra.EventHandlers.AspNetCore/Endpoints/AttributeCollectionSubmitEndpoint.cs
@@ -1,5 +1,7 @@
-﻿using Entra.EventHandlers.AspNetCore.Adapters;
-using Entra.EventHandlers.AspNetCore.Base;
+﻿using Entra.EventHandlers.Abstractions.Events;
+using Entra.EventHandlers.Abstractions.Responses;
+using Entra.EventHandlers.AspNetCore.Abstractions;
+using Entra.EventHandlers.AspNetCore.Adapters;
 using Entra.EventHandlers.Hosting.Resolvers;
 
 namespace Entra.EventHandlers.AspNetCore.Endpoints;
@@ -8,7 +10,9 @@ public sealed class AttributeCollectionSubmitEndpoint(
     ILogger<AttributeCollectionSubmitEndpoint> logger,
     IRequestAdapter requestAdapter,
     IResponseAdapter responseAdapter,
-    IEntraEventHandlerResolver resolver) : AttributeCollectionSubmitEndpointBase(logger, requestAdapter, responseAdapter, resolver)
+    IEntraEventHandlerResolver resolver)
+    : EntraTypedEndpointBase<AttributeCollectionSubmitEvent, AttributeCollectionSubmitResponse>(
+        logger, requestAdapter, responseAdapter, resolver)
 {
     public override void Map(IEndpointRouteBuilder endpoints)
     {
