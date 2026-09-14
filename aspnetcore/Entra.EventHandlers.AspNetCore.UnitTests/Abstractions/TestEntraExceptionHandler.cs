f2d90e5200c17fed93b47708c28fc90b7da886a3
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-09-14 08:46:33 +0200

    chore: one liner

--- aspnetcore/Entra.EventHandlers.AspNetCore.UnitTests/Abstractions/TestEntraExceptionHandler.cs
+++ aspnetcore/Entra.EventHandlers.AspNetCore.UnitTests/Abstractions/TestEntraExceptionHandler.cs
@@ -5,10 +5,7 @@ namespace Entra.EventHandlers.AspNetCore.UnitTests.Abstractions;
 
 internal sealed class TestEntraExceptionHandler : IEntraExceptionHandler
 {
-    public bool WasCalled
-    {
-        get; private set;
-    }
+    public bool WasCalled { get; private set; }
 
     public Task HandleAsync(Exception ex, HttpContext context, bool isEntraException)
     {
