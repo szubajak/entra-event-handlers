16d35be48f67b61814a33766357ba3ac49d3ef0b
Jakub Szubarga <jakub.szubarga@gmail.com>
2026-09-25 09:08:18 +0200

    Security Unit Tests

--- core/Entra.EventHandlers.UnitTests/Handlers/Base/AttributeCollectionStartHandlerBaseTests.cs
+++ core/Entra.EventHandlers.UnitTests/Handlers/Base/AttributeCollectionStartHandlerBaseTests.cs
@@ -97,7 +97,7 @@ public class AttributeCollectionStartHandlerBaseTests
         _sut.CoreTest.ShouldThrow = true;
 
         // Act
-        var result = await _sut.HandleAsync(evt, CancellationToken.None);
+        var result = await _sut.HandleAsync(evt, TestContext.Current.CancellationToken);
 
         // Assert
         _sut.CoreTest.HandleCoreCallCount.Should().Be(1);
@@ -128,7 +128,7 @@ public class AttributeCollectionStartHandlerBaseTests
         var evt = TestEvents.CreateAttributeCollectionStartEvent(_fixture, valid: false);
 
         // Act
-        var result = await _sut.HandleAsync(evt, CancellationToken.None);
+        var result = await _sut.HandleAsync(evt, TestContext.Current.CancellationToken);
 
         // Assert
         _sut.CoreTest.HandleCoreCallCount.Should().Be(0);
--- security/Entra.EventHandlers.Security/Decryptors/KeyVaultPasswordContextDecryptor.cs
+++ security/Entra.EventHandlers.Security/Decryptors/KeyVaultPasswordContextDecryptor.cs
@@ -16,11 +16,11 @@ public sealed class KeyVaultPasswordContextDecryptor(IKeyVaultCertificateProvide
     {
         ArgumentException.ThrowIfNullOrEmpty(encryptedPasswordContext);
 
-        var rsa = await _certificateProvider.GetRsaAsync(cancellationToken)
-            ?? throw new EntraSecurityException("The RSA key required to decrypt the password context is unavailable.");
-
         try
         {
+            var rsa = await _certificateProvider.GetRsaAsync(cancellationToken)
+                ?? throw new EntraSecurityException("The RSA key required to decrypt the password context is unavailable.");
+
             var decrypted = JWT.Decode(encryptedPasswordContext, rsa);
 
             var json = JWT.Decode(decrypted, null, JwsAlgorithm.none);
@@ -37,14 +37,21 @@ public sealed class KeyVaultPasswordContextDecryptor(IKeyVaultCertificateProvide
                 Password = payload.Password
             };
         }
+        catch (OperationCanceledException)
+        {
+            throw;
+        }
+        catch (EntraException)
+        {
+            throw;
+        }
+        catch (JsonException ex)
+        {
+            throw new EntraDeserializationException("Unable to deserialize the decrypted password context.", ex);
+        }
         catch (Exception ex)
         {
-            throw ex switch
-            {
-                JsonException jex => new EntraDeserializationException("Unable to deserialize the decrypted password context.", jex),
-                EntraDeserializationException => ex,
-                _ => new EntraSecurityException("Unable to decrypt the password context.", ex)
-            };
+            throw new EntraSecurityException("Unable to decrypt the password context.", ex);
         }
     }
 }
